ad295e22f1af80adce6f42b46d06f562c8c6e670
YehorIvanov <124167578+YehorIvanov@users.noreply.github.com>
2023-07-22 16:47:07 +0300

    add current day indication

--- scripts/calendar/header.js
+++ scripts/calendar/header.js
@@ -1,30 +1,33 @@
 import { getItem } from "../common/storage.js";
-import { generateWeekRange } from "../common/time.utils.js";
+import { generateWeekRange, isInDisplayedWeek } from "../common/time.utils.js";
 import { openModal } from "../common/modal.js";
 
-const daysOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 export const renderHeader = () => {
   const calendarHeaderElem = document.querySelector(".calendar__header");
-  calendarHeaderElem.innerHTML = '';
+  calendarHeaderElem.innerHTML = "";
   const week = generateWeekRange(getItem("displayedWeekStart"));
   week.forEach((num, index) => {
+    const dayNameElem = document.createElement("span");
+    dayNameElem.innerHTML = daysOfWeek[index];
+    dayNameElem.classList.add("day-label__day-name");
 
-  const dayNameElem = document.createElement("span");
-  dayNameElem.innerHTML = daysOfWeek[index];
-  dayNameElem.classList.add("day-label__day-name");
+    const dayNamberElem = document.createElement("span");
+    dayNamberElem.innerHTML = num.getDate();
+    dayNamberElem.classList.add("day-label__day-namber");
+    if (
+      isInDisplayedWeek(new Date()) &&
+      num.getDate() === new Date().getDate()
+    ) {
+      dayNamberElem.classList.add("day-label__day-namber_current");
+    }
 
-  const dayNamberElem = document.createElement("span");
-  dayNamberElem.innerHTML = num.getDate();
-  dayNamberElem.classList.add("day-label__day-namber");
-
-  const dayLabelElem = document.createElement("div");
-  dayLabelElem.classList.add("calendar__day-label", "day-label");
-  dayLabelElem.append(dayNameElem, dayNamberElem);
-  calendarHeaderElem.appendChild(dayLabelElem);
+    const dayLabelElem = document.createElement("div");
+    dayLabelElem.classList.add("calendar__day-label", "day-label");
+    dayLabelElem.append(dayNameElem, dayNamberElem);
+    calendarHeaderElem.appendChild(dayLabelElem);
   });
-
-
 
   // на основе displayedWeekStart из storage с помощью generateWeekRange сформируйте массив дней текущей недели
   // на основе полученного массива сформируйте разметку в виде строки - 7 дней (день недели и число в месяце)
@@ -34,6 +37,6 @@ export const renderHeader = () => {
 
 // при клике на кнопку "Create" открыть модальное окно с формой для создания события
 // назначьте здесь обработчик
-const createEventBtnElem = document.querySelector('.create-event-btn');
-const openModalBinded = openModal.bind(null,{});
-createEventBtnElem.addEventListener('click', openModalBinded);
+const createEventBtnElem = document.querySelector(".create-event-btn");
+const openModalBinded = openModal.bind(null, {});
+createEventBtnElem.addEventListener("click", openModalBinded);
